76855615c820fa97a2068ff8a6618781acde1694
nimol <limnimol.dev@gmail.com>
2026-03-11 15:47:41 +0700

    Done tructure

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxtjs/google-fonts',
+    '@vesp/nuxt-fontawesome'
+  ],
+  
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  
+
+  
+  
+});
