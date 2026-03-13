6bb3f81ad64a23c72c38e5bf61553c92a3ebbfc5
nimol <limnimol.dev@gmail.com>
2026-03-13 09:32:48 +0700

    feat/att done 97%

--- app/pages/attendance.vue
+++ app/pages/attendance.vue
@@ -1,7 +1,9 @@
 <script setup>
 
+    import AttendanceCard from '../components/AttendanceCard.vue';
     import AttendanceFilter from '../components/AttendanceFilter.vue';
     import AttendanceTable from '../components/AttendanceTable.vue';
+
 
 </script>
 
@@ -17,80 +19,11 @@
         </div>
 
         
+        <AttendanceCard />
         
-        <div class="flex items-center gap-[85px]">
-            
-            <div class=" mt-5 rounded-md border border-gray-300 p-[22px] border-2 h-[130px] w-[255px] boder-[#333]">
-                
-                <div class="flex items-center  gap-[10px]">
-                        
-                    <Icon name="si-glyph:square-checked" class="text-2xl " />
-
-                    <p class="font-medium text-lg"> Present Today</p>
-                    
-                </div>
-
-                <p class="font-medium text-lg  mt-3">  40 </p>
-                <p class=" text-[#4d4d4d]">134 People Remaining</p>
-                
-            </div>
-
-            <div class=" mt-5 rounded-md border border-gray-300 p-[22px] border-2 h-[130px] w-[255px] boder-[#333]">
-                
-                <div class="flex items-center  gap-[10px]">
-                        
-                    <Icon name="si-glyph:alarm-clock" class="text-2xl " />
-
-                    <p class="font-medium text-lg"> Late Entry</p>
-                    
-                </div>
-
-                <p class="font-medium text-lg  mt-3">  26 </p>
-                <p class=" text-[#4d4d4d]">13 People are on time</p>
-                
-            </div>
-
-            <div class=" mt-5 rounded-md border border-gray-300 p-[22px] border-2 h-[130px] w-[255px] boder-[#333]">
-                
-                <div class="flex items-center  gap-[10px]">
-                        
-                    <Icon name="si-glyph:circle-forward" class="text-2xl " />
-
-                    <p class="font-medium text-lg"> On Leave</p>
-                    
-                </div>
-
-                <p class="font-medium text-lg  mt-3">  12 </p>
-                <p class=" text-[#4d4d4d]">Approved leave</p>
-                
-            </div>
-            
-            <div class=" mt-5 rounded-md border border-gray-300 p-[22px] border-2 h-[130px] w-[255px] boder-[#333]">
-                
-                <div class="flex items-center  gap-[10px]">
-                        
-                    <Icon name="line-md:alert-circle" class="text-2xl " />
-
-                    <p class="font-medium text-lg"> Absent</p>
-                    
-                </div>
-
-                <p class="font-medium text-lg  mt-3">  03 </p>
-                <p class=" text-[#4d4d4d]">Wihtout informing</p>
-                
-            </div>
-
-        </div>
-
         <AttendanceFilter />
 
         <AttendanceTable />
-
-        
-        
-        
-        
-
 
 
     </div>
