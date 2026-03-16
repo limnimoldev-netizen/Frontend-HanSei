d03309e3effb75dec113028e988a047123679429
xzidx <c.samnang.de@gmail.com>
2026-03-16 16:47:05 +0700

    not done yet still have more function

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,16 +7,16 @@
                                 <div class=" w-[350px] h-[100px] flex justify-center items-center gap-5">
                                             <div class="bg-primary w-auto p-[10px] h-[35px] rounded-lg flex flex-row justify-center items-center gap-2 border border-white">
                                             <Icon name="mdi:account-outline" class="text-2xl text-white" />
-                                            <span class="text-base text-white">Online: <span class="font-bold">98</span> </span>
+                                            <span class="text-base text-white">Online: <span class="font-bold">142</span> </span>
                                             </div>
                                             <div class="bg-red-600 w-auto p-[10px] h-[35px] rounded-lg flex flex-row justify-center items-center gap-2 border border-white">
                                             <Icon name="mdi:inbox-outline" class="text-2xl text-white" />
-                                            <span class="text-base text-white">Offline: <span class="font-bold">579</span> </span>
+                                            <span class="text-base text-white">Offline: <span class="font-bold">1093</span> </span>
                                             </div>
                                             
                                               
                                 </div>
-                               
+                
                                 
             </div>
              <div class="bg-primary w-full h-[550px] flex justify-evenly items-center">
@@ -61,7 +61,7 @@
                         </span>
 
                         <span class="text-7xl font-bold text-red-500 my-4">
-                            1093
+                            900
                         </span>
 
                         <div class="flex items-center gap-2 bg-red-50 px-3 py-1 rounded-full border border-green-100">
@@ -76,7 +76,7 @@
                         </div>
 
                         <div class="flex items-center gap-2  px-3 py-1 rounded-base  mt-[5px]">
-                            <span class="text-black text-base font-bold uppercase">1093/1235 <span class="text-gray-500">Employees</span></span>
+                            <span class="text-black text-base font-bold uppercase">900/1235 <span class="text-gray-500">Employees</span></span>
                         </div>
 
                     </div>
@@ -88,16 +88,16 @@
                         </div>
 
                         <span class="text-primary font-medium text-lg uppercase tracking-wide">
-                            Absent Employees
+                            Pending Approvals
                         </span>
 
-                        <span class="text-7xl font-bold text-red-500 my-4">
-                            1093
+                        <span class="text-7xl font-bold text-primary my-4">
+                            193
                         </span>
 
                         <div class="flex items-center gap-2 bg-red-50 px-3 py-1 rounded-full border border-green-100">
-                            <div class="h-2 w-2 rounded-full bg-red-500"></div>
-                            <span class="text-red-700 text-xs font-bold uppercase">Unactive </span>
+                            <div class="h-2 w-2 rounded-full bg-green-500"></div>
+                            <span class="text-green-700 text-xs font-bold uppercase">notification </span>
                             
                             
                         </div>
@@ -107,7 +107,7 @@
                         </div>
 
                         <div class="flex items-center gap-2  px-3 py-1 rounded-base  mt-[5px]">
-                            <span class="text-black text-base font-bold uppercase">1093/1235 <span class="text-gray-500">Employees</span></span>
+                            <span class="text-black text-base font-bold uppercase">193/1235 <span class="text-gray-500">Employees</span></span>
                         </div>
 
                     </div>
