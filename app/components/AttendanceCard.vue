5ab0cb774e4ddb68a437ad2c071f515341432812
limnimoldev-netizen <git config --global user.email limnimol.dev@gmail.com>
2026-03-23 03:52:54 +0700

    done

--- app/components/AttendanceCard.vue
+++ app/components/AttendanceCard.vue
@@ -1,118 +1,61 @@
 <template>
-     
-
-   
-    <div class="flex items-center gap-[25px]">
-
-        <div class=" mt-5 rounded-md border border-gray-300 p-[18px] border-2 h-[130px] w-[240px] boder-[#333]">
-
+  
+    <div class="flex items-center gap-[20px] flex-wrap">
+        
+        <div v-for="item in stats" :key="item.title"class="mt-5 rounded-xl border-2 border-gray-200 p-[18px] h-[130px] w-[240px]  ">
             
+            <div class="flex items-center gap-[10px]">
                 
-            <div class="flex items-center  gap-[10px]">
-                        
-                <Icon name="mdi:account-multiple" class="text-2xl " />
-                <p class="font-medium text-lg"> Employee</p>   
+                <Icon :name="item.icon" class="text-2xl text-gray-700" />
+                
+                <p class="font-medium text-lg text-gray-800">{{ item.title }}</p>
+            
             </div>
 
-            <p class="font-medium text-lg  mt-3">  1304 </p>
-            <p class=" text-[#4d4d4d]">Wihtout informing</p>
-                
-        </div>  
+            <p class="text-3xl font-black text-gray-900 mt-2">{{ item.value }}</p>
 
-        <div class=" mt-5 rounded-md border border-gray-300 p-[18px] border-2 h-[130px] w-[240px] boder-[#333]">
-
-                
-            <div class="flex items-center  gap-[10px]">
-                        
-                <Icon name="si-glyph:square-checked" class="text-2xl " />
-                <p class="font-medium text-lg"> Present Today</p>
-                    
-
-            </div>
-
-            <p class="text-3xl font-black text-gray-900 mt-2">{{ card.value }}</p>
-            <p class="text-sm text-gray-400 font-medium">{{ card.subtext }}</p>
+            <p class="text-sm text-gray-400 font-medium">{{ item.subtext }}</p>
 
         </div>
-
-
-        <div class=" mt-5 rounded-md border border-gray-300 p-[18px] border-2 h-[130px] w-[240px] boder-[#333]">
-                
-            <div class="flex items-center  gap-[10px]">
-                            
-                <Icon name="mdi:alarm-note-off" class="text-2xl " />
-                <p class="font-medium text-lg"> Late Entry</p>
-                        
-            </div>
-
-            <p class="font-medium text-lg  mt-3">  26 </p>
-            <p class=" text-[#4d4d4d]">13 People are on time</p>
-                
-        </div>
-
-        <div class=" mt-5 rounded-md border border-gray-300 p-[18px] border-2 h-[130px] w-[240px] boder-[#333]">
-
-
-                
-            <div class="flex items-center  gap-[10px]">
-                        
-                <Icon name="mdi:account-arrow-left" class="text-2xl " />
-                <p class="font-medium text-lg"> On Leave</p>        
-            </div>
-
-                <p class="font-medium text-lg  mt-3">  12 </p>
-                <p class=" text-[#4d4d4d]">Approved leave</p>
-                
-        </div>
-            
-        <div class=" mt-5 rounded-md border border-gray-300 p-[18px] border-2 h-[130px] w-[240px] boder-[#333]">
-
-            
-                
-            <div class="flex items-center  gap-[10px]">
-                        
-                <Icon name="mdi:account-cancel" class="text-2xl " />
-                <p class="font-medium text-lg"> Absent</p>   
-            </div>
-
-            <p class="font-medium text-lg  mt-3">  03 </p>
-            <p class=" text-[#4d4d4d]">Wihtout informing</p>
-                
-        </div>    
 
     </div>
 
 </template>
 
 <script setup>
+
 const stats = [
+
+  { title: 'Employee', 
+    value: '1304', 
+    subtext: 'Total Staff', 
+    icon: 'mdi:account-multiple' },
+
 
   { title: 'Present Today', 
     value: '40', 
     subtext: '134 People Remaining', 
-    icon: 'si-glyph:square-checked'
-
- },
+    icon: 'si-glyph:square-checked' },
 
   { title: 'Late Entry', 
     value: '26', 
     subtext: '13 People are on time', 
-    icon: 'si-glyph:alarm-clock' 
-},
+    icon: 'mdi:alarm-note-off' },
+
 
   { title: 'On Leave', 
     value: '12', 
     subtext: 'Approved leave', 
-    icon: 'si-glyph:circle-forward'
+    icon: 'mdi:account-arrow-left' },
 
- },
 
   { title: 'Absent', 
     value: '03', 
     subtext: 'Without informing', 
-    icon: 'line-md:alert-circle'
+    icon: 'mdi:account-cancel' }
 
- }
+
 ]
+
 
 </script>
