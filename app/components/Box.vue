e901eedfe38f68a66e40dc795d3d8c5b4e491bb2
xzidx <c.samnang.dev@gmail.com>
2026-03-23 22:36:00 +0700

    fix the bug

---
+++ app/components/Box.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  img: {
+    type: String,
+    default: ''
+  },
+
+  boxClasses: {
+    type: String,
+    default: 'px-4 py-2 rounded-xl  w-70 h-40 flex justify-center items-center shadow-2xl gap-2'
+  }
+})
+</script>
+
+<template>
+  <div :class="['box-container', boxClasses]">
+
+  
+      <img :src="img" alt="" class="w-full h-full object-cover rounded-lg" />
+    
+
+    <slot />
+  </div>
+</template>
